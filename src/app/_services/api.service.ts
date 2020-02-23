9b1894e816b9753b3e96dd97def5f538f2214f01
Lee Bryant <contact@leecbryant.com>
2020-02-23 13:10:44 -0800

    created base api

---
+++ src/app/_services/api.service.ts
@@ -0,0 +1,26 @@
+// Core angular
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable,  } from 'rxjs';
+// Custom
+import { environment } from '../../environments/environment';
+import { Hall } from '../_interfaces/hall.interface';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Accept: 'application/json; odata=verbose',
+    'Content-Type': 'application/json; odata=verbose'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class APIService {
+  constructor(private http: HttpClient) { }
+
+  getHalls(): Observable<Hall> {
+    return this.http.get<Hall>(environment.serverName + 'api/base/halls', );
+  }  
+}
