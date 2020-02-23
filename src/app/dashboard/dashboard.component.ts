4060fcabf810790a5f0a1a3ad3f2ef7c134a3a0f
Lee Bryant <contact@leecbryant.com>
2020-02-23 13:11:19 -0800

    currency modal

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ThemeConstants } from '../shared/config/theme-constant';
+import { SnackBar } from '../_services/notification.service';
 
 @Component({
     templateUrl: 'dashboard.html'
@@ -7,35 +7,14 @@ import { ThemeConstants } from '../shared/config/theme-constant';
 
 export class DashboardComponent implements OnInit {
 
-    constructor() { }
-
-    public navigation: any;
-
-    multiSelectOptions: any = [
-        {
-            label: 'Lee Bryant',
-            value: 'Lee Bryant',
-        }
-    ];
-
-    multiSelectConfig: any = {
-        delimiter: ',',
-        labelField: 'label',
-        valueField: 'value',
-        searchField: ['label'],
-        maxItems: 200,
-        persist: false,
-        create: function(input) {
-            return {
-                label: input,
-                value: input
-            };
-        }
-    };
-
-    multiSelectValue: string[] = ['reactjs'];
-
-    ngOnInit()  {
+    constructor(private snackbar: SnackBar) {
     }
 
+    ngOnInit() {
+        
+    }
+
+    addCurrency(form) {
+        console.log(form.value)
+    }
 }
