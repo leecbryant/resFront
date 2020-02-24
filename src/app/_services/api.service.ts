eee0fee815d2d854f9a483f8a63a948de658542c
Lee Bryant <contact@leecbryant.com>
2020-02-23 19:30:07 -0800

    student db implementation

--- src/app/_services/api.service.ts
+++ src/app/_services/api.service.ts
@@ -5,6 +5,7 @@ import { Observable,  } from 'rxjs';
 // Custom
 import { environment } from '../../environments/environment';
 import { Hall } from '../_interfaces/hall.interface';
+import { Student } from '../_interfaces/student.interface';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -23,4 +24,15 @@ export class APIService {
   getHalls(): Observable<Hall> {
     return this.http.get<Hall>(environment.serverName + 'api/base/halls', );
   }  
+
+  getStudents(): Observable<Student> {
+    return this.http.get<Student>(environment.serverName + 'api/base/students', );
+  }
+
+  newStudent(data): Observable<any> {
+    return this.http.post<any>(environment.serverName + 'api/base/students', data, {
+      reportProgress: true,
+      observe: 'response'
+    });
+  }
 }
