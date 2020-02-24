7d4448de928a375ac5ee6b7ddec7c7b2e3609b43
Lee Bryant <contact@leecbryant.com>
2020-02-23 17:08:58 -0800

    base dialog for inputing study

---
+++ src/app/_dialogs/trackers/trackstudy.dialog.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-studytracker-dialog',
+  templateUrl: './trackstudy.dialog.html'
+})
+export class TrackStudyDialog {
+  message: string = ""
+  cancelButtonText = "Cancel"
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<TrackStudyDialog>) {
+    if (data) {
+      this.message = data.message || this.message;
+      if (data.buttonText) {
+        this.cancelButtonText = data.buttonText.cancel || this.cancelButtonText;
+      }
+    }
+    this.dialogRef.updateSize('300vw','300vw')
+  }
+
+  onConfirmClick(): void {
+    this.dialogRef.close(true);
+  }
+
+}
